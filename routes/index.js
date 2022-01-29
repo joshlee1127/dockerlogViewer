680fd4ed4dece15d5dc372fc162f1f369108472d
joshlee <joshlee@m1.mac>
2022-01-29 20:03:26 +0800

    add log range

--- routes/index.js
+++ routes/index.js
@@ -14,7 +14,7 @@ router.get("/", async function (req, res, next) {
 router.get("/listalllog/:id", async (req, res, next) => {
     const id = req.params.id
     // const execCommand = "docker ps -q | xargs -L 1 docker logs"
-    const execCommand = "docker logs " + id
+    const execCommand = "docker logs --since 10m" + id
     try {
         const { stdout, stderr } = await exec(execCommand)
         if (stdout) {
