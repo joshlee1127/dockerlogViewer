c1e557fcf3ae8962e6e2eca2e97a60ccf042a492
joshlee <joshlee@m1.mac>
2022-01-29 20:10:24 +0800

    tail 1000

--- routes/index.js
+++ routes/index.js
@@ -14,7 +14,7 @@ router.get("/", async function (req, res, next) {
 router.get("/listalllog/:id", async (req, res, next) => {
     const id = req.params.id
     // const execCommand = "docker ps -q | xargs -L 1 docker logs"
-    const execCommand = "docker logs --since 10m " + id
+    const execCommand = "docker logs --tail 1000 " + id
     try {
         const { stdout, stderr } = await exec(execCommand)
         if (stdout) {
